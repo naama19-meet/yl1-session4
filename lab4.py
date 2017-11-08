c72e82232da2cbe7e742a2a0e8506fbd31c95240
moheeeldin19-meet <moheeeldin19@meet.mit.edu>
2017-11-08 19:16:29 +0200

    lab4work

---
+++ lab4.py
@@ -0,0 +1,39 @@
+#class Animal(object):
+	"""docstring for ClassName"""
+	#def __init__(self, color , favorit_food, name, age,sound):
+		#self.color = color
+	#	self.favorit_food = favorit_food
+		#self.name = name
+		#self.age = age
+		#self.sound = sound
+
+	#def description(self):
+		#print("the name of the dog is" + self.name + "sharon is" + self.age + "years old")	
+		#print("the sound of the cat is " + self.sound  + self.sound)
+	
+
+#cat = Animal("black", "pizza" ,  "sharon" , "15" ,"miouuu")
+#cat.description()
+
+
+class person(object):
+	"""docstring for ClassName"""
+	def __init__(self, name, age, city, gender, favorit_food):
+		self.name = name
+		self.age = age
+		self.city = city
+		self.gender = gender
+
+	def eat(self,favorit_food ):
+
+		pass
+		
+
+
+
+
+		
+		
+		
+		
+		
